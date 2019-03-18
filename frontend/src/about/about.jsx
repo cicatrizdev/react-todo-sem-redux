460696896eda6b504febb4319bc3728bd099db41
cicatrizwp <pedrocmello.bsi@gmail.com>
2019-03-18 01:06:24 -0300

    Se não vai ser no amor, então vou aprender pelo ódio ¬¬

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="Sobre" small="Nós"></PageHeader>
+        <h2>História</h2>
+        <p>
+        Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl. Quem num gosta di mé, boa gentis num é. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!
+
+        Copo furadis é disculpa de bebadis, arcu quam euismod magna. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Quem num gosta di mim que vai caçá sua turmis! Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.
+
+        Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Casamentiss faiz malandris se pirulitá. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum. Detraxit consequat et quo num tendi nada. 
+        </p>
+        <h2>Missão e Visão</h2>
+        <p>
+        Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Diuretics paradis num copo é motivis de denguis. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.
+
+        Atirei o pau no gatis, per gatis num morreus. Manduma pindureta quium dia nois paga. Viva Forevis aptent taciti sociosqu ad litora torquent. Per aumento de cachacis, eu reclamis. 
+        </p>
+        <h2>Contatos</h2>
+        <p>
+        Paisis, filhis, espiritis santis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. A ordem dos tratores não altera o pão duris. 
+        </p>
+    </div>
+);
